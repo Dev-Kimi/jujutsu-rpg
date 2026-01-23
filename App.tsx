46fd1ba68ff9cc16e5aebe1e55442cc6401df485
Dev-Kimi <kamateus976@gmail.com>
2026-01-23 17:46:07 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1102,8 +1102,8 @@ const App: React.FC = () => {
                     {tab === 'techniques' && 'Técnicas'}
                     {tab === 'inventory' && 'Invent.'}
                     {tab === 'progression' && 'Progressão'}
-                    {tab === 'campaigns' && 'Campanhas'}
                     {tab === 'binding-vows' && 'Votos'}
+                    {tab === 'campaigns' && 'Campanhas'}
                     </button>
                 ))}
             </div>
